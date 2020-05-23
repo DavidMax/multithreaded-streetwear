e5091cedaa1f4b88c51ebeb85677efa91c120866
DavidMax <davidjamesmax@gmail.com>
2020-05-23 01:46:19 -0700

    Add node-sass; stub dir & menu components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Home from './components/pages/home/home.component'
+
 function App() {
   return (
     <div>
-
+      <Home />
     </div>
   );
 }
---
+++ src/components/pages/home/home.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.styles.scss'
+
+import Directory from '../../directory/directory.component'
+
+const Home = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default Home;
