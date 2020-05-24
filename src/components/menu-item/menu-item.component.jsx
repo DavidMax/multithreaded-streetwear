c3d19f1896961d61babf479870d8051861a275a8
DavidMax <davidjamesmax@gmail.com>
2020-05-24 15:47:58 -0700

    Match base url of detail pages in menu

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -8,7 +8,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
         style={{
             backgroundImage: `url(${imageUrl})`
         }}
-        onClick={() => history.push(`${linkUrl}`)}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
         className={`${size} menu-item`}>
         <div className='content'>
             <h1 className='title'>{title}</h1>
