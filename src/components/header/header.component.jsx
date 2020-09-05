599620aebbd9412eb4c5b0f49b3d59e46f95e2be
DavidMax <davidjamesmax@gmail.com>
2020-09-04 18:44:22 -0700

    Build header and app-bar menu

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import AppBar from './app-bar/app-bar.component'
+
+const Header = () => (
+    <div>
+        <AppBar />
+    </div>
+);
+
+export default Header;
